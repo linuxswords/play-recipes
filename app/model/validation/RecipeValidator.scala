d1de93826b1c0b984f458f0d84c0e49823bb3dd5
Martin Knoller Stocker <martin.knoller@gmail.com>
2014-03-31 21:58:59 +0200

    format fails because it is not strictly symmetric

--- app/model/validation/RecipeValidator.scala
+++ app/model/validation/RecipeValidator.scala
@@ -1,8 +1,8 @@
 package model.validation
 
-import model.serialize.SerializeProvider._
 import play.api.libs.json._
 import play.api.libs.json.Reads._
+import play.api.libs.json.Writes._
 import play.api.libs.functional.syntax._
 import model.{Recipe, Ingredient}
 
@@ -11,21 +11,39 @@ import model.{Recipe, Ingredient}
  */
 object RecipeValidator {
 
-  implicit val ingredientReads: Format[Ingredient] = (
-    (__ \ "amount").format[BigDecimal](min(BigDecimal(0))) and
-      (__ \ "name").format[String](minLength[String](2)) and
-      (__ \ "unit").format[Option[String]]
+  implicit val ingredientWrites: Writes[Ingredient] = (
+    (__ \ "amount").write[BigDecimal] and
+      (__ \ "name").write[String] and
+      (__ \ "unit").write[Option[String]]
+    )(unlift(Ingredient.unapply))
+
+  implicit val ingredientReads: Reads[Ingredient] = (
+    (__ \ "amount").read[BigDecimal](min(BigDecimal(0))) and
+      (__ \ "name").read[String](minLength[String](2)) and
+      (__ \ "unit").read[Option[String]]
     )(Ingredient.apply _)
 
-  implicit val recipeformats: Format[Recipe] = (
-    (__ \ "title").format[String](minLength[String](2)) and
-      (__ \ "alternateTitle").format[Option[String]] and
-      (__ \ "instruction").format[Option[String]] and
-      (__ \ "comment").format[Option[String]] and
-      (__ \ "ingredients").format[List[Ingredient]] and
-      (__ \ "keywords").format[Option[List[String]]] and
-      (__ \ "timeRequired").format[Option[BigDecimal]] and
-      (__ \ "url").format[Option[String]]
+  implicit val recipewrites: Writes[Recipe] = (
+    (__ \ "title").write[String] and
+      (__ \ "alternateTitle").write[Option[String]] and
+      (__ \ "instruction").write[Option[String]] and
+      (__ \ "comment").write[Option[String]] and
+      (__ \ "ingredients").write[List[Ingredient]] and
+      (__ \ "keywords").write[Option[List[String]]] and
+      (__ \ "timeRequired").write[Option[BigDecimal]] and
+      (__ \ "url").write[Option[String]]
+    )(unlift(Recipe.unapply))
+
+  implicit val recipereads: Reads[Recipe] = (
+    (__ \ "title").read[String](minLength[String](2)) and
+      (__ \ "alternateTitle").read[Option[String]] and
+      (__ \ "instruction").read[Option[String]] and
+      (__ \ "comment").read[Option[String]] and
+      (__ \ "ingredients").read[List[Ingredient]] and
+      (__ \ "keywords").read[Option[List[String]]] and
+      (__ \ "timeRequired").read[Option[BigDecimal]] and
+      (__ \ "url").read[Option[String]]
     )(Recipe.apply _)
+
 
 }
